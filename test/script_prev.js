f1a012dbc669e2e8521c5a542f537f7da40f0305
Celes <tkdals0617@gachon.ac.kr>
2025-08-20 17:02:36 +0900

    25.08.20 : 실제 좌표 값을 기반으로 자동적으로 위치를 추정하여 contour를 그리는 과정 제작 중

--- test/script_prev.js
+++ test/script_prev.js
@@ -1,210 +1,121 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ratio = 1;
+// // Path 1
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(100, 200);
+// ctx.lineTo(50, 50);
+// ctx.closePath();
 
-// 좌표들은 예시로 넣은 것이며, 실제 좌표는 필요에 따라 수정
-
-// 1. Soft tissue landmarks
-soft_tissue_glabella = [487, 21];
-soft_tissue_nasion = [466, 109];
-pronasale = [550, 260];
-subnasale = [496, 304];
-soft_tissue_subspinale = [521, 355];
-labrale_superius = [527, 378];
-stomion = [500, 407];
-labrale_inferius = [510, 435];
-soft_tissue_submentale = [465, 481];
-soft_tissue_pogonion = [460, 544];
-soft_tissue_gnathion = [443, 577];
-
-// 2. Maxilla landmarks
-sup_inci = [471, 421];
-prosthion = [466, 364];
-sup_api = [425, 318];
-
-inf_inci = [442, 398];
-infradentale = [431, 440];
-inf_api = [394, 476];
-
-max_1st = [338, 363];
-man_1st = [328, 382];
-
-ans = [461, 298];
-pns = [244, 276];
+// ctx.fillStyle = '#92c88cff';
+// ctx.fill();
+// ctx.stroke();
 
 
-//3. Mandible landmarks
+// // Path 2
+// ctx.beginPath();
+// ctx.moveTo(200, 50);
+// ctx.lineTo(150, 200);
+// ctx.lineTo(250, 200);
+// // ctx.closePath();
+// ctx.stroke();
 
 
+// // Path 3
+// ctx.beginPath();
+// // ctx.rect(300, 50, 100, 100);
+// center_x = canvas.width / 4;
+// center_y = canvas.height / 3;
+// ctx.arc(center_x, center_y, 50, 0, Math.PI, false);
+// // ctx.fillStyle = '#90bfc8ff';
+// // ctx.fill();
+// ctx.stroke();
 
 
-//////////////////////////////////////
+// // Path 4 (Test for complex curves)
+// ctx.moveTo(50, 400);
+// ctx.fillStyle = '#90bfc8ff';
+// ctx.bezierCurveTo(100, 300, 200, 500, 300, 400);
+// ctx.bezierCurveTo(400, 300, 500, 500, 600, 400);
+// // ctx.closePath();
+// ctx.stroke();
 
-// 얼굴의 가장 가장자리 part
+
+// // Path 5 (Test for quadratic curves)
+// ctx.beginPath();
+// ctx.moveTo(50, 500);
+// ctx.quadraticCurveTo(420, 370, 540, 400);
+// ctx.quadraticCurveTo(600, 410, 660, 480);
+// ctx.quadraticCurveTo(680, 540, 600, 560);
+// ctx.quadraticCurveTo(620, 600, 560, 600);
+// ctx.quadraticCurveTo(500, 630, 510, 570);
+// ctx.quadraticCurveTo(480, 540, 520, 510);
+// ctx.quadraticCurveTo(460, 510, 460, 430);
+// ctx.quadraticCurveTo(400, 420, 350, 400);
+// ctx.stroke();
+
+
+// Path 6 (Mask 0)
 const points = [
-    { x: soft_tissue_glabella[0],
-      y: soft_tissue_glabella[1] },
+    { x: 487, y: 21 },
 
     //////////////////
     // 중점 1
-    { x: (soft_tissue_glabella[0]+soft_tissue_nasion[0])/2,
-      y: (soft_tissue_glabella[1]+soft_tissue_nasion[1])/2 },
+    { x: 475, y: 61 },
     //////////////////
     
-    { x: soft_tissue_nasion[0],
-      y: soft_tissue_nasion[1] },
+    { x: 466, y: 109 },
 
     //////////////////
-    // 중점 2 : Soft Tissue Nasion ~ Pronasale 간 3등분해서 1/3씩 느낌으로
-    { x: soft_tissue_nasion[0] + (pronasale[0]-soft_tissue_nasion[0])/3,
-      y: soft_tissue_nasion[1] + (pronasale[1]-soft_tissue_nasion[1])/3 },
-    { x: soft_tissue_nasion[0] + (pronasale[0]-soft_tissue_nasion[0])*2/3,
-      y: soft_tissue_nasion[1] + (pronasale[1]-soft_tissue_nasion[1])*2/3 },
+    // 중점 2 : 3등분해서 1/3씩 느낌으로
+    { x: 489, y: 161 },
+    { x: 521, y: 207 },
     //////////////////
 
-    { x: pronasale[0],
-      y: pronasale[1] },
+    { x: 550, y: 260 },
 
     //////////////////
-    // 중점 3 (Pronasale과 Subnasale의 중간점)
-    { x: (pronasale[0]+subnasale[0])/2,
-      y: (pronasale[1]+subnasale[1])/2 },
+    // 중점 3
+    { x: 523, y: 282 },
     //////////////////
 
-    { x: subnasale[0],
-      y: subnasale[1] },
+    { x: 496, y: 304 },
 
     //////////////////
-    // 중점 4
-    { x: (subnasale[0]+soft_tissue_subspinale[0])/2,
-      y: (subnasale[1]+soft_tissue_subspinale[1])/2 },
+    // 중점 3
+    { x: 508, y: 330 },
     //////////////////
 
 
-    { x: soft_tissue_subspinale[0],
-      y: soft_tissue_subspinale[1] },
-    { x: labrale_superius[0],
-      y: labrale_superius[1] },
-    { x: stomion[0],
-      y: stomion[1] },
+    { x: 521, y: 355 },
+    { x: 527, y: 378 },
+    { x: 500, y: 407 },
 
-    { x: stomion[0],
-      y: stomion[1] },
-    { x: labrale_inferius[0],
-      y: labrale_inferius[1] },
-    { x: soft_tissue_submentale[0],
-      y: soft_tissue_submentale[1] },
-    { x: soft_tissue_pogonion[0],
-      y: soft_tissue_pogonion[1] },
-    { x: soft_tissue_gnathion[0],
-      y: soft_tissue_gnathion[1] },
+    { x: 500, y: 407 },
+    { x: 510, y: 435 },
+    { x: 465, y: 481 },
+    { x: 460, y: 544 },
+    { x: 443, y: 577 },
     
 ];
 
-
-
-// 윗니 앞니
-const points2 = [
-  { x: sup_inci[0],
-    y: sup_inci[1] },
-
-  // 평행사변형(마름모) : D=C+(A−B)
-  // { x: 439, y: 386 },
-  { x: (sup_inci[0] + sup_api[0] - prosthion[0]),
-    y: (sup_inci[1] + sup_api[1] - prosthion[1]) },
-  
-  { x: sup_api[0],
-    y: sup_api[1] },
-
-  { x: prosthion[0],
-    y: prosthion[1] },
-  
-  ////////////////
-  // { x: 475, y: 393 },
-  { x: (prosthion[0]+sup_inci[0])/2,
-    y: (prosthion[1]+sup_inci[1])/2 },
-  ////////////////
-
-  { x: sup_inci[0],
-    y: sup_inci[1] },
-]
-
 // 아랫니 앞니
-const points3 = [
-  { x: inf_inci[0],
-    y: inf_inci[1] },
-  
-  // { x: 441, y: 420 },
-  { x: (inf_inci[0] + infradentale[0]) / 2,
-    y: (inf_inci[0] + infradentale[0]) / 2 },
-  
-  { x: infradentale[0],
-    y: infradentale[1] },
-  { x: inf_api[0],
-    y: inf_api[1] },
+const points2 = [
+  { x: 442, y: 397 },
+  { x: 441, y: 420 },
+  { x: 431, y: 440 },
+  { x: 394, y: 476 },
 
-  ////////////////
-  // { x: 408, y: 430 },
-  temp_x = (inf_inci[0]+inf_api[0]) - infradentale[0],
-  temp_y = (inf_inci[1]+inf_api[1]) - infradentale[1],
-  { x: temp_x,
-    y: temp_y },
-  ////////////////
-  
-  // 중점
-  // { x: 422, y: 410 },
-  { x: (temp_x + inf_inci[0]) / 2,
-    y: (temp_y + inf_inci[1]) / 2 },
-  
-  { x: inf_inci[0],
-    y: inf_inci[1] },
-]
-
-
-// Maxilla
-const points4 = [
-  { x: pns[0],
-    y: pns[1] },
-
-  { x: 375, y: 279 },
-  { x: 417, y: 269 },
-  { x: 439, y: 285 },
-  
-  { x: ans[0],
-    y: ans[1] },
-]
-
-const points5 = [
-  // { x: 461, y: 298 },
-  { x: ans[0],
-    y: ans[1] },
-
-  { x: 450, y: 307 },
-  { x: 448, y: 321 },
-  { x: 466, y: 364 },
-]
-
-
-const points6 = [
-  // { x: 439, y: 386 },
-  { x: (sup_inci[0] + sup_api[0] - prosthion[0]),
-    y: (sup_inci[1] + sup_api[1] - prosthion[1]) },
-  
-  // { x: 375, y: 323 },
-  // PNS와 [439, 386] 사이의 중점
-  { x: ( pns[0] + (sup_inci[0] + sup_api[0] - prosthion[0]) ) / 2 + 20*ratio,
-    y: ( pns[1] + (sup_inci[1] + sup_api[1] - prosthion[1]) ) / 2 - 20*ratio},
-
-  
-  // { x: 244, y: 276 },
-  { x: pns[0],
-    y: pns[1] },
+  { x: 408, y: 430 },
+  { x: 432, y: 397 },
+  { x: 442, y: 397 },
 ]
 
 
 // Mandible
-const points7 = [
+const points3 = [
   { x: 431, y: 440 },
   { x: 412, y: 469 },
   { x: 407, y: 529 },
@@ -218,13 +129,52 @@ const points7 = [
   { x: 408, y: 430 },
 ]
 
-const points8 = [
+const points4 = [
   { x: 352, y: 555 },
   { x: 274, y: 502 },
   { x: 195, y: 445 },
   { x: 104, y: 405 },
   { x: 97, y: 305 },
   { x: 87, y: 210 },
+]
+
+
+// Maxilla
+const points5 = [
+  { x: 244, y: 276 },
+  { x: 375, y: 279 },
+  { x: 417, y: 269 },
+  { x: 439, y: 285 },
+  { x: 461, y: 298 },
+]
+
+const points6 = [
+  { x: 461, y: 298 },
+  { x: 450, y: 307 },
+  { x: 448, y: 321 },
+  { x: 466, y: 364 },
+]
+
+
+const points7 = [
+  { x: 439, y: 386 },
+  { x: 375, y: 323 },
+  { x: 244, y: 276 },
+]
+
+
+// 윗니 앞니
+const points8 = [
+  { x: 471, y: 421 },
+  { x: 439, y: 386 },
+  { x: 425, y: 318 },
+  { x: 466, y: 364 },
+  
+  ////////////////
+  { x: 475, y: 393 },
+  ////////////////
+
+  { x: 471, y: 421 },
 ]
 
 
